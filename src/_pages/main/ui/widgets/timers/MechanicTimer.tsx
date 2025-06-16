48019a893d6085993eb77b7fd7f26c96cd4831bc
Bahaidahar <krutoyBaha@bk.ru>
2025-06-16 12:38:42 +0500

    fix

---
+++ src/_pages/main/ui/widgets/timers/MechanicTimer.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { IUser } from "@/shared/models/types/user";
+import { useMechanicTimer } from "../modals/user/hooks/useMechanicTimer";
+import { ICar } from "@/shared/models/types/car";
+
+interface WaitingTimerProps {
+  user: IUser;
+  deCar?: ICar;
+  className?: string;
+  deReservationTime?: string;
+}
+
+export const MechanicWaitingTimer = ({
+  user,
+  deCar,
+  className = "",
+  deReservationTime,
+}: WaitingTimerProps) => {
+  const car = deCar != null ? deCar : user.current_rental!.car_details;
+  const reservationTime =
+    deReservationTime != null
+      ? deReservationTime
+      : user.current_rental!.rental_details!.reservation_time || "";
+
+  const { timeLeft, isExpired } = useMechanicTimer(reservationTime);
+
+  return car.owned_car ? null : (
+    <div
+      className={`${
+        isExpired ? "bg-red-500" : "bg-[#1D77FF]"
+      } p-3 px-4 rounded-[40px] flex flex-row justify-between items-center ${className}`}
+    >
+      <div className="flex flex-row gap-2 items-center text-[16px] text-white">
+        {isExpired ? "Время истекло" : "Таймер"}
+      </div>
+      <span className="text-[16px] text-[#fff] font-semibold">{timeLeft}</span>
+    </div>
+  );
+};
