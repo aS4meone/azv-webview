4781ac5a1c9ee6e84fa1293b47b014414ac8f084
Bahaidahar <krutoyBaha@bk.ru>
2025-06-17 16:43:39 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const config: NextConfig = {
+  reactStrictMode: false,
   images: {
     domains: ["api.azvmotors.kz"],
   },
