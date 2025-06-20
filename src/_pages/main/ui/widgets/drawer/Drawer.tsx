d2444f91c8c9638c370cc820b6e37ab4b424aa72
Bahaidahar <krutoyBaha@bk.ru>
2025-06-20 23:09:55 +0500

    fix

--- src/_pages/main/ui/widgets/drawer/Drawer.tsx
+++ src/_pages/main/ui/widgets/drawer/Drawer.tsx
@@ -81,7 +81,7 @@ const Drawer = () => {
 
       <div
         className={cn(
-          "fixed flex flex-col justify-between pb-10 top-0 left-0 h-full w-80 bg-[#191919] transform transition-transform duration-300 ease-in-out z-30",
+          "fixed flex flex-col justify-between pb-10 top-0 left-0 h-full w-80 bg-[#191919] transform transition-transform duration-300 ease-in-out z-20",
           isDrawerOpen ? "translate-x-0" : "-translate-x-full"
         )}
       >
